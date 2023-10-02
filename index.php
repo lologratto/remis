106c6d422e56e0630b39c5cab51d1e512237f553
lorenzo <lorenzograttone@gmail.com>
2023-10-02 19:57:17 -0300

    2/10 7:57

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $cabe = " ";
   <header id="header" class="header d-flex align-items-center">
 
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="index.php" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
         <h1>Remis<span>.</span></h1>
